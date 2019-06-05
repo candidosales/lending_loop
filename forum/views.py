1cbf8377e7baffd918bba40f821eb7b05fb1f304
Cândido <candidosg@gmail.com>
2019-06-05 00:28:55 -0400

    fix navbar

--- forum/views.py
+++ forum/views.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from django.urls import reverse
 from django.views import generic
 
-
 from .models import Topic, Comment
 # Create your views here.
 class IndexView(generic.ListView):
